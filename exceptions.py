c4fa8a40a612b644e6ba06d8a62bd400776fdd85
Ihor Pedan <ihorpedan@gmail.com>
2024-08-20 14:09:16 +0200

    Removed test exception

--- exceptions.py
+++
@@ -1,3 +0,0 @@
-class MentoverseException(Exception):
-    def __init__(self, name: str):
-        self.name = name
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI, Request
-from exceptions import MentoverseException
 from router import user, course, appointments
 from db import models
 from db.database import engine
@@ -12,12 +11,5 @@ app.include_router(user.router)
 app.include_router(course.router)
 app.include_router(appointments.router)
 
-
-@app.exception_handler(MentoverseException)
-def mentoverse_exception_handler(request: Request, exc: MentoverseException):
-    return JSONResponse(
-        status_code=418,
-        content={'detail': exc.name}
-    )
 
 models.Base.metadata.create_all(engine)
