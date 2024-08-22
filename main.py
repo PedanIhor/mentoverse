592dae1d1f38488a37ab0bd499899787933b2a1c
Ihor Pedan <ihorpedan@gmail.com>
2024-08-22 16:44:22 +0200

    Added ability to run server with debugger

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from db import models
 from db.database import engine
 from fastapi.responses import JSONResponse
 from auth import authentication
+import uvicorn
 
 app = FastAPI()
 app.include_router(authentication.router)
@@ -13,3 +14,6 @@ app.include_router(appointments.router)
 
 
 models.Base.metadata.create_all(engine)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host="localhost", port=8000, reload=True)
