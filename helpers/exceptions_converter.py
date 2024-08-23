9b98dca0923eb9ec34cfd1594a2b9e105c9c93b2
Ihor Pedan <ihorpedan@gmail.com>
2024-08-23 13:58:53 +0200

    Added exceptions_converter decorator function

--- helpers/exceptions_converter.py
+++ helpers/exceptions_converter.py
@@ -1,5 +1,7 @@
 from db.db_exceptions import DbException, DbExceptionReason
 from fastapi import HTTPException, status
+from functools import wraps
+from helpers.functions import contains_explicit_return
 
 
 class ExceptionsConverter:
@@ -14,11 +16,25 @@ class ExceptionsConverter:
         match self.exception.reason:
             case DbExceptionReason.NOT_FOUND:
                 exc = HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                                     detail=self.exception.detail)
+                                    detail=self.exception.detail)
             case DbExceptionReason.FORBIDDEN:
                 exc = HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                                     detail=self.exception.detail)
+                                    detail=self.exception.detail)
             case DbExceptionReason.ACTION_IMPOSSIBLE:
                 exc = HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                                     detail=self.exception.detail)
+                                    detail=self.exception.detail)
         return exc
+
+
+def exceptions_converter(action):
+    @wraps(action)
+    def wrapper(*args, **kwargs):
+        try:
+            if contains_explicit_return(action):
+                return action(*args, **kwargs)
+            else:
+                action(*args, **kwargs)
+        except DbException as e:
+            raise ExceptionsConverter(e).http()
+    return wrapper
+
