0fd950d730b1c3b235b827eb120cfe130cab5048
Ihor Pedan <ihorpedan@gmail.com>
2024-08-23 14:02:36 +0200

    Replaced nested functions in routers with a decorator

--- router/appointment_participation.py
+++ router/appointment_participation.py
@@ -4,7 +4,7 @@ from db.database import get_db
 from sqlalchemy.orm import Session
 from db import db_appointment
 from schemas import AppointmentParticipationBase
-from helpers.db_action_wrapper import try_db_action
+from helpers.exceptions_converter import exceptions_converter
 
 router = APIRouter(
     prefix='/appointmentParticipation',
@@ -14,22 +14,22 @@ router = APIRouter(
 
 # Adds student to the appointment
 @router.post('/', status_code=status.HTTP_201_CREATED)
+@exceptions_converter
 def create_appointment_participation(
         request: AppointmentParticipationBase,
         db: Session = Depends(get_db),
         current_user: CurrentUser = Depends(get_current_user)
 ):
-    def action():
-        if current_user.id is not request.student_id:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-        return db_appointment.assign_student_for_appointment(
-            db, request.appointment_id, request.student_id
-        )
-    return try_db_action(action)
+    if current_user.id is not request.student_id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    return db_appointment.assign_student_for_appointment(
+        db, request.appointment_id, request.student_id
+    )
 
 
 # Removes student's participation from appointment
 @router.delete('/{appointment_id}/{student_id}', status_code=status.HTTP_204_NO_CONTENT)
+@exceptions_converter
 def delete_student_participation(
         appointment_id: int,
         student_id: int,
--- router/user.py
+++ router/user.py
@@ -5,7 +5,7 @@ from schemas import UserBase, UserDisplay, UserBaseForPatch
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from auth.oauth2 import get_current_user, CurrentUser
-from helpers.db_action_wrapper import try_db_action
+from helpers.exceptions_converter import exceptions_converter
 
 router = APIRouter(
     prefix='/user',
@@ -27,66 +27,60 @@ def get_all_users(db: Session = Depends(get_db)):
 
 # Read user with id
 @router.get('/{id}', response_model=UserDisplay)
+@exceptions_converter
 def get_user_by_id(id: int, db: Session = Depends(get_db)):
-    def action():
-        return db_user.get_user_by_id(db, id)
-    return try_db_action(action)
+    return db_user.get_user_by_id(db, id)
 
 
 # Update user
 @router.put('/{id}', response_model=UserDisplay)
+@exceptions_converter
 def update_user(
         id: int,
         request: UserBase,
         db: Session = Depends(get_db),
         current_user: CurrentUser = Depends(get_current_user)
 ):
-    def action():
-        if id is not current_user.id:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-        else:
-            return db_user.update_user(db, id, request)
-    return try_db_action(action)
+    if id is not current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    else:
+        return db_user.update_user(db, id, request)
 
 
 @router.patch('/{id}', response_model=UserDisplay)
+@exceptions_converter
 def update_user_by_patch(
         id: int,
         request: UserBaseForPatch,
         db: Session = Depends(get_db),
         current_user: CurrentUser = Depends(get_current_user)
 ):
-    def action():
-        if id is not current_user.id:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-        else:
-            db_user.update_by_patch(db, id, request)
-            return db_user.get_user_by_id(db, id)
-
-    return try_db_action(action)
+    if id is not current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    else:
+        db_user.update_by_patch(db, id, request)
+        return db_user.get_user_by_id(db, id)
 
 
 @router.post('/{user_id}/password', response_model=UserDisplay)
+@exceptions_converter
 def update_password(
         user_id: int,
         password: str,
         db: Session = Depends(get_db),
         current_user: CurrentUser = Depends(get_current_user)
 ):
-    def action():
-        if user_id is not current_user.id:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-        else:
-            return db_user.update_user_password(db, user_id, password)
-    return try_db_action(action)
+    if user_id is not current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    else:
+        return db_user.update_user_password(db, user_id, password)
 
 
 # Delete user
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@exceptions_converter
 def delete_user(id: int, db: Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
-    def action():
-        if id is not current_user.id:
-            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-        else:
-            db_user.delete_user(db, current_user.id)
-    try_db_action(action)
+    if id is not current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
+    else:
+        db_user.delete_user(db, current_user.id)
